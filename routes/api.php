006bfde5f5d2c7c1d7ed6e70292f510698c7acbf
Samir Qureshi <qureshisamar351@gmail.com>
2024-11-29 17:47:44 +0530

    initial commit

--- routes/api.php
+++ routes/api.php
@@ -27,19 +27,22 @@ Route::post('/create-user-account',[UserController::class,'userCreateAccount']);
 Route::get('get-user-details',[UserController::class,'getUserDetails']);
 Route::get('fetch-weba-info/{wabaid}',[UserController::class,'fetchWeba']);
 
-<<<<<<< HEAD
-//Nikita Apis (29-11-2024)
-Route::post('create-template-location/{wabaid}',[TemplateController::class,'createTemplateLocation']);
-Route::post('create-template-text/{wabaid}',[TemplateController::class,'createTemplateText']);
-Route::post('create-template-copy-code/{wabaid}',[TemplateController::class,'createTemplateCopyCode']);
-Route::post('create-template-catalog/{wabaid}',[TemplateController::class,'createTemplateCatalog']);
-Route::post('create-template-mpm/{wabaid}',[TemplateController::class,'createTemplateMpm']);
-=======
+
+//create template
 Route::post('/template-multiple-button/{wabaid}', [UserController::class, 'sendTemplate']);
 Route::post('/create-template-carousel/{wabaid}', [UserController::class, 'carouselTemplate']);
 Route::post('/create-template-image/{wabaid}', [UserController::class, 'imageTemplate']);
 Route::post('/create-template-video/{wabaid}', [UserController::class, 'createTemplateVideo']);
 Route::post('/create-template-document/{wabaid}', [UserController::class, 'createTemplateDocument']);
 
+// (29-11-2024)
+Route::post('create-template-location/{wabaid}',[TemplateController::class,'createTemplateLocation']);
+Route::post('create-template-text/{wabaid}',[TemplateController::class,'createTemplateText']);
+Route::post('create-template-copy-code/{wabaid}',[TemplateController::class,'createTemplateCopyCode']);
+Route::post('create-template-catalog/{wabaid}',[TemplateController::class,'createTemplateCatalog']);
+Route::post('create-template-mpm/{wabaid}',[TemplateController::class,'createTemplateMpm']);
 
->>>>>>> de63665637f3465dd10fe98b31f6717da4cf1c01
+
+
+
+
